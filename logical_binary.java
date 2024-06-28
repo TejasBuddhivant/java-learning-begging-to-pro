2bfc6ffd094f77b86bf62a97b47b4301ab163f39
TejasGB <123950873+TejasGB@users.noreply.github.com>
2024-06-28 19:56:12 +0530

    date.28.6.2024
    today i learn about logical operators and unary operator and i also learn new things

---
+++ logical_binary.java
@@ -0,0 +1,51 @@
+class logical_binary{
+public static void main(String arg[]){
+int a=1 , b=0;
+System.out.println(a&b);
+a=2;
+b=0;
+System.out.println(a&b);
+int a=3 , b=0;
+System.out.println(a&b);
+int a=4 , b=0;
+System.out.println(a&b);
+int a=5 , b=0;
+System.out.println(a&b);
+int a=6 , b=0;
+System.out.println(a&b);
+int a=7 , b=0;
+System.out.println(a&b);
+int a=8 , b=0;
+System.out.println(a&b);
+int a=9 , b=0;
+System.out.println(a&b);
+int a=10 , b=0;
+System.out.println(a&b);
+
+System.out.println();
+System.out.println();
+
+
+System.out.println(a&b);
+int a=2 , b=1;
+System.out.println(a&b);
+int a=3 , b=1;
+System.out.println(a&b);
+int a=4 , b=1;
+System.out.println(a&b);
+int a=5 , b=1;
+System.out.println(a&b);
+int a=6 , b=1;
+System.out.println(a&b);
+int a=7 , b=1;
+System.out.println(a&b);
+int a=8 , b=1;
+System.out.println(a&b);
+int a=9 , b=1;
+System.out.println(a&b);
+int a=10 , b=1;
+System.out.println(a&b);
+}
+}
+/* 
+1& 0 = 0*/
---
+++ unari_ope.java
@@ -0,0 +1,27 @@
+class Unari_ope{
+public static void main(String arr[]){
+int a= 3;
+//++a;
+//System.out.println(a);
+//a++;
+//System.out.println(a);
+//++a;
+//a++;
+//System.out.println(a++);
+//System.out.println(a++);
+//System.out.println(a++);
+int res = a++ + a++;
+
+System.out.println(a);
+a=11;
+System.out.println(a++ + a++);
+//System.out.println(a);
+//a=3;
+//a++;
+//System.out.println(a);
+//a++;
+//System.out.println(a);
+
+//System.out.println(a+a);
+}
+}
