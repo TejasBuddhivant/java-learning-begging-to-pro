bb6f7559ba878647f978fc66382e926a24762380
TejasGB <123950873+TejasBuddhivant@users.noreply.github.com>
2024-07-16 20:09:50 +0530

    update file

---
+++ advance_switch.java
@@ -0,0 +1,13 @@
+class advance_switch{
+/* today i learn about the advance switch case and you can not use two or three statement at time but this is use good code formatting according to my knowledge but i always learn about this letter so this is advance switch*/
+public static void main(String arr[]){
+int num= 4;
+switch(num){
+case 1 ->System.out.println("it is one");
+case 2 ->System.out.println("it is two");
+case 3 ->System.out.println("it is three");
+case 4 ->System.out.println("it is four");
+default ->System.out.println("it is not a number");
+}
+}
+}
