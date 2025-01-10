b5ec43603e4f8c1ec1be3b25e4d4449eef5460b4
TejasGB <123950873+TejasBuddhivant@users.noreply.github.com>
2025-01-10 12:34:24 +0530

    updating new js learnings

--- front end/js/script.js
+++ front end/js/script.js
@@ -1,37 +1,102 @@
 // document.write("hello world !!!");
 
 //console.log("lavdya");
-var a=10;
-//console.log("this is first a of var",a);
-var a=25;
-//console.log("this is second var a",a);
+// var a=10;
+// //console.log("this is first a of var",a);
+// var a=25;
+// //console.log("this is second var a",a);
 
           
-let ab=10;
-//console.log("this is let ab",ab);
-//let ab=23;
-// this is not valid syntax for declaring let value 
-ab = "ssssssss";
-ab = 5;
+// let ab=10;
+// //console.log("this is let ab",ab);
+// //let ab=23;
+// // this is not valid syntax for declaring let value 
+// ab = "ssssssss";
+// ab = 5;
 
-const abc= 100;
-// this is use for final value 
-// abc=300; this will give us a error in this example because it is not valid to reinnsialise the const value
-//console.log(abc);
-//console.log(abc);
-//console.log(ab);
+// const abc= 100;
+// // this is use for final value 
+// // abc=300; this will give us a error in this example because it is not valid to reinnsialise the const value
+// //console.log(abc);
+// //console.log(abc);
+// //console.log(ab);
 
-// object  cration  with JS
-let ARR = ["GTA vice " ," GTA CJ  " ,"GTA VI"  ];
- console.log(ARR);
+// // object  cration  with JS
+// let ARR = ["GTA vice " ," GTA CJ  " ,"GTA VI"  ];
+//  console.log(ARR);
 
 
- let Obj =  {
-   name:'tejas',
-   per:32 ,
- };
+//  let Obj =  {
+//    name:'tejas',
+//    per:32 ,
+//  };
 
-// date
-var today = new  Date;
-console.log(today);
- console.log(Obj);
+// // date
+// var today = new  Date;
+// console.log(today);
+//  console.log(Obj);
+
+// A = 40;
+// AB =  20;
+// console.log(A!=AB);
+// console.log(A);
+
+//   var B = 7;
+// //console.let(B);
+// console.log(++B);
+// //console.log(B);
+// B++;
+// --B;
+// B--;
+// console.log(B);
+
+// let somnath = " GTA san anddreas";
+
+
+// if(somnath == " GTA vice city "){
+//   console.log("this is GTA vice city");
+// }
+// else{
+//   console.log("this is not GTA vice city");
+// }
+
+
+
+// let a=3;
+// switch(a){
+//   case 1:console.log("no is 1");break;
+//   case 2:console.log("no is 2");break;
+//   case 3:console.log("no is 3");break;
+//   default: console.log("invalid");
+// }
+
+// for(let i=0;i<=10;i++){
+//   console.log(i);
+// }
+
+// in java there are three types of function 
+
+// 1) is named function 
+
+// function sqare(num){
+//   console.log("square root of "+num+"is :"+num*num);
+// }
+// sqare(55);
+
+// 2) is anonymous function 
+
+// const anonymousfunc = function(i){
+//   console.log("square root of "+i+"is :"+i*i);
+// }
+// anonymousfunc(4);
+
+
+// 3) is arrow function this is popular function declaration and it's wildly used 
+// const Aroowfunc = (i)=>{
+//   //this is arrow function it is simple to use and this is replication of anonymous function
+
+//   console.log("hello there "+i);
+
+// }
+// Aroowfunc(5);
+
